dc783e3310f621f41fc48145719755084d1a50d0
Arnav Katyayan <ishumisra99@gmail.com>
2026-05-06 18:55:57 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, useTheme } from './context/ThemeContext';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import InterviewPage from './pages/InterviewPage';
 import './App.css';
+
+const AppContent = () => {
+  const { colors, toggleTheme } = useTheme();
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        background: colors.background,
+        color: colors.text,
+      }}
+    >
+      <Header onThemeToggle={toggleTheme} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/interview" element={<InterviewPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </ThemeProvider>
   );
 }
 
